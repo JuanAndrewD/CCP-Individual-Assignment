d6dd8343e0b1798157501b663ca97975a5563dd4
JuanAndrewD <juanandrewdj@gmail.com>
2026-04-26 17:40:07 +0800

    Defined functions needed and updated DrinkType class

---
+++ src/main/java/com/ccp/gogocoffee/tp078726/DrinkType.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ccp.gogocoffee.tp078726;
+
+/**
+ *
+ * @author juana
+ */
+public class DrinkType {
+    private final int price;
+    private final String name;
+    
+    DrinkType(int price, String name) {
+        this.price = price;
+        this.name = name;
+    }
+    
+    public int getPrice() { return price; }
+    public String getName() { return name; }
+}
